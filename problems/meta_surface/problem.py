96203d669b324e29f04b76995f08bc7cc0598ce0
hyin <h.yin@liacs.leidenuniv.nl>
2025-12-10 06:37:49 +0000

    clean problem

--- problems/meta_surface/problem.py
+++ problems/meta_surface/problem.py
@@ -4,18 +4,18 @@ import sys
 import cv2
 import ioh
 import torch
-import joblib
 import numpy as np
 sys.path.insert(0, os.getcwd())
-from sklearn.decomposition import PCA
 from problems.meta_surface.wideresnet import WideResNet
 # fmt: on
 
 
 class meta_surface:
-    def __init__(self, RT_path="problems/meta_surface/model_best_RT.pth.tar",
-                 IT_path="problems/meta_surface/model_best_IT.pth.tar",
-                 device="cuda:0"):
+    def __init__(self, triangle_edge_length: int = 18,
+                 RT_path: str = 'problems/meta_surface/model_best_RT.pth.tar',
+                 IT_path: str = 'problems/meta_surface/model_best_IT.pth.tar',
+                 device: str = 'cuda:0'):
+        self.triangle_edge_length = triangle_edge_length
         self.RT_path = RT_path
         self.IT_path = IT_path
         self.device = device
@@ -31,27 +31,17 @@ class meta_surface:
         checkpoint = torch.load(self.IT_path, weights_only=True)
         self.regressor_imaginary.load_state_dict(checkpoint['state_dict'])
         self.regressor_imaginary.eval()
-        self.dim = int(18*19/2)
-        self.lb = np.array([-1. for _ in range(self.dim)])
+        self.dim = int(self.triangle_edge_length *
+                       (self.triangle_edge_length + 1) / 2)
+        self.lb = np.array([0. for _ in range(self.dim)])
         self.ub = np.array([1. for _ in range(self.dim)])
-        # X_pca_train = np.random.uniform(self.lb, self.ub,
-        #                                 size=(1000*self.dim, self.lb.shape[0]))
-        # self.pca_dim = 20
-        # self.pca = PCA(n_components=self.pca_dim)
-        # self.X_pca = self.pca.fit_transform(X_pca_train)
-        # pca_min = np.min(self.X_pca, axis=0)
-        # pca_max = np.max(self.X_pca, axis=0)
-        # self.pca_lb = pca_min - 0.1
-        # self.pca_ub = pca_max + 0.1
 
     def __call__(self, x):
-        # x = self.pca.inverse_transform(x)
-        x = np.where(x <= 0, 0., 1.)
+        x = np.where(x <= 0.5, 0., 1.)
         triangle = self.vector_to_triangle(x)
-        square_18 = self.reflect_triangle(triangle)
-        square_36 = self.rotate_around_corner(square_18)
-        # x_in = self.create_final_image(square_18).reshape((1, 1, 36, 36))
-        x_in = square_36.reshape((1, 1, 36, 36))
+        quarter_square = self.reflect_triangle(triangle)
+        square = self.rotate_around_corner(quarter_square)
+        x_in = self.create_final_image(square).reshape((1, 1, 36, 36))
         x_in = torch.tensor(x_in).float().to(self.device)
         logits_re = self.regressor_real(x_in)  # 1x100
         logits_im = self.regressor_imaginary(x_in)  # 1x100
@@ -74,47 +64,42 @@ class meta_surface:
         return triangle
 
     def reflect_triangle(self, triangle):
-        square_9 = triangle.copy()
+        quarter_square = triangle.copy()
         for i in range(18):
             for j in range(i + 1, 18):
-                square_9[i, j] = triangle[j, i]
-        return square_9
+                quarter_square[i, j] = triangle[j, i]
+        return quarter_square
 
-    def rotate_around_corner(self, square_9):
-        size = square_9.shape[0] * 2
-        square_18 = np.zeros((size, size))
-        rotated_90 = np.rot90(square_9)
-        rotated_180 = np.rot90(square_9, 2)
-        rotated_270 = np.rot90(square_9, 3)
-        square_18[:square_9.shape[0], :square_9.shape[1]] = square_9
-        square_18[:square_9.shape[0], square_9.shape[1]:] = rotated_270
-        square_18[square_9.shape[0]:, square_9.shape[1]:] = rotated_180
-        square_18[square_9.shape[0]:, :square_9.shape[1]] = rotated_90
-        return square_18
+    def rotate_around_corner(self, quarter_square):
+        size = quarter_square.shape[0] * 2
+        square = np.zeros((size, size))
+        rotated_90 = np.rot90(quarter_square)
+        rotated_180 = np.rot90(quarter_square, 2)
+        rotated_270 = np.rot90(quarter_square, 3)
+        square[:quarter_square.shape[0],
+               :quarter_square.shape[1]] = quarter_square
+        square[:quarter_square.shape[0],
+               quarter_square.shape[1]:] = rotated_270
+        square[quarter_square.shape[0]:,
+               quarter_square.shape[1]:] = rotated_180
+        square[quarter_square.shape[0]:,
+               :quarter_square.shape[1]] = rotated_90
+        return square
 
-    def create_final_image(self, square_18):
-        square_36 = cv2.resize(square_18, (36, 36))
-        square_36 = np.where(square_36 <= 0, -1., 1.)
+    def create_final_image(self, square):
+        square_36 = cv2.resize(square, (36, 36))
+        square_36 = np.where(square_36 <= 0.5, 0., 1.)
         return square_36
-        # square_36 = np.zeros((36, 36))
-        # for i in range(2):
-        #     for j in range(2):
-        #         start_i = i * 18
-        #         end_i = (i + 1) * 18
-        #         start_j = j * 18
-        #         end_j = (j + 1) * 18
-        #         square_36[start_i:end_i, start_j:end_j] = square_18
-        # return square_36
 
 
 def get_meta_surface_problem(
-        RT_path="problems/meta_surface/model_best_IT.pth.tar",
-        IT_path="problems/meta_surface/model_best_IT.pth.tar",
-        device="cuda:0"):
+        RT_path='problems/meta_surface/model_best_IT.pth.tar',
+        IT_path='problems/meta_surface/model_best_IT.pth.tar',
+        device='cuda:0'):
     prob = meta_surface(RT_path=RT_path, IT_path=IT_path, device=device)
-    ioh.problem.wrap_real_problem(prob, name="meta_surface",
+    ioh.problem.wrap_real_problem(prob, name='meta_surface',
                                   optimization_type=ioh.OptimizationType.MIN)
-    problem = ioh.get_problem("meta_surface", dimension=prob.dim)
+    problem = ioh.get_problem('meta_surface', dimension=prob.dim)
     problem.bounds.lb = prob.lb
     problem.bounds.ub = prob.ub
     return problem
