f9f5d72c8fe8658f3b2654cf17fa9fc199c7c469
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2026-03-11 23:35:31 +0100

    update

--- scripts/ela_ablation/_file_check.py
+++ scripts/ela_ablation/_file_check.py
@@ -31,3 +31,29 @@ if __name__ == "__main__":
                         counter_missing += 1
                         print(f"Missing file: {file_name}")
         print(f"Missing {counter_missing} out of {counter_total} files in total.")
+    elif task_id == 1:
+        seeds = list(range(100))
+        block_coefs = np.linspace(2.0, 5.0, 31)
+        n_coefs = np.logspace(np.log10(10), np.log10(1000), 30).astype(int)
+        counter_total = 0
+        counter_missing = 0
+        for problem_id in problem_ids:
+            problem_name = ProblemName(problem_id)
+            for ela_set_id in range(7):
+                for seed in seeds:
+                    if ela_set_id in [10, 11, 12]:
+                        for block_coef in block_coefs:
+                            counter_total += 1
+                            file_name = f"{problem_name}-{ela_set_id}-seed:{seed}-block_coef:{block_coef:.1f}-n_coef:{0}.csv"
+                            if not os.path.exists(f"data/Ablation_ELA/atom/{file_name}"):
+                                counter_missing += 1
+                                # print(f"Missing file: {file_name}")
+                    else:
+                        for n_coef in n_coefs:
+                            counter_total += 1
+                            file_name = f"{problem_name}-{ela_set_id}-seed:{seed}-block_coef:{0.0}-n_coef:{n_coef}.csv"
+                            if not os.path.exists(f"data/Ablation_ELA/atom/{file_name}"):
+                                counter_missing += 1
+                                # print(f"Missing file: {file_name}")
+        print(f"Missing {counter_missing} out of {counter_total} files in total.")
+        
